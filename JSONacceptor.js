2d7bf6a064ce0413dc6f18c6deec8bd4974d5c64
chanstaples1 <36214341+chanstaples1@users.noreply.github.com>
2018-05-15 21:00:40 -0400

    JSON program

--- JSONacceptor.js
+++ JSONacceptor.js
@@ -3,13 +3,18 @@ class JSONaccepter{
         this._index = index;
         this._parkid = parkid;
     }
-    executeProgram(){
+    //Index will be hard-coded based on page user is on.
+    //Size is user-defined by input from front-end
+    //Parkid is pulled from HTML of park page.
+    //GET request to get the JSON file.
+    //POST request is to post to the database via same url. 
+    captureReviewData(){
         var json = (function () {
             var json = null;
             $.ajax({
                 'async': false,
                 'global': false,
-                'url': my_url,
+                'url': "https://national-parks.fcgit.net/api/es?index=review&size=10&parkid=1",
                 'dataType': "json",
                 'success': function (data) {
                     json = data;
@@ -18,5 +23,4 @@ class JSONaccepter{
             return json;
         })(); 
     }
-    
 }
